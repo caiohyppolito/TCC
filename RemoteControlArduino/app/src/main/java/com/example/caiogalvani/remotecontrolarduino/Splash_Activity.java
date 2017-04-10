4cbde5c1806ea4983774e4b132123618cda38023
Bruno Barreto <bruno.barreto@directfacil.com.br>
2017-04-09 21:55:59 -0300

    Tela de splash criada, precisa corrigir o layout da imagem, inserido o icone do App, para avaliação

--- RemoteControlArduino/app/src/main/java/com/example/caiogalvani/remotecontrolarduino/Splash_Activity.java
+++ RemoteControlArduino/app/src/main/java/com/example/caiogalvani/remotecontrolarduino/Splash_Activity.java
@@ -1,8 +1,13 @@
 package com.example.caiogalvani.remotecontrolarduino;
 
-/**
- * Created by bruno.barreto on 08/04/2017.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class Splash_Activity {
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_);
+    }
 }
