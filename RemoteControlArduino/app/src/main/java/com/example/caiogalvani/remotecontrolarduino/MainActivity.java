65dececf397907662168e756be1d0cd00ad06a66
Bruno Barreto <bruno.barreto@directfacil.com.br>
2017-04-10 15:13:27 -0300

    atualizado splash screen

--- RemoteControlArduino/app/src/main/java/com/example/caiogalvani/remotecontrolarduino/MainActivity.java
+++ RemoteControlArduino/app/src/main/java/com/example/caiogalvani/remotecontrolarduino/MainActivity.java
@@ -43,6 +43,11 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
